c741755929fffb5e4a597c8e7e9ced6cabd9893f
Snowel Pereira <snoweel.pererira@google.com>
2024-06-30 18:27:46 -0230

    refactor code into components

--- src/index.js
+++ src/index.js
@@ -102,33 +102,40 @@ function Pizza({ pizzaObject }) {
   );
 }
 
+function Order({ closeHour }) {
+  return (
+    <div className="order">
+      <p>
+        We are currently open till {closeHour}:00. Come visit us or Order online
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+function ClosedMessage({ openHour, closeHour }) {
+  return (
+    <div className="order">
+      <p>
+        {" "}
+        We are happy to welcome you between {openHour}:00 and {closeHour}:00
+      </p>
+    </div>
+  );
+}
+
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 9;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour < closeHour;
-  console.log(hour);
-  const message = isOpen ? "we are currently open" : "we are currently closed";
-  //   alert(message);
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We are currently open till {closeHour}:00. Come visit us or Order
-            online
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
-        <div className="order">
-          <p>
-            {" "}
-            We are happy to welcome you between {openHour}:00 and {closeHour}:00
-          </p>
-        </div>
+        <ClosedMessage closeHour={closeHour} openHour={openHour} />
       )}
-      {/* {new Date().toLocaleTimeString()}: {message} */}
     </footer>
   );
 }
